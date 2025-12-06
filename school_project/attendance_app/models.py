1f64611dd63caaed127739cf242af427853984f3
kaykayag <you@example.com>
2025-12-06 19:24:49 +0800

    feat: added Spinner

--- school_project/attendance_app/models.py
+++ school_project/attendance_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Student(models.Model):
     # Matches your frontend requirements
-    student_id = models.CharField(max_length=20, unique=True)
+    student_id = models.CharField(max_length=10, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
@@ -12,7 +12,7 @@ class Student(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 class Teacher(models.Model):
-    teacher_id = models.CharField(max_length=20, unique=True)
+    teacher_id = models.CharField(max_length=10, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     subject = models.CharField(max_length=100)
